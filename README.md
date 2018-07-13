2a39a99805d0c0da73f902d9964ecff70ca872ab
Mathias Maciossek <mathias.maciossek@gmail.com>
2018-07-13 12:06:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+[![Build Status](https://travis-ci.org/hft-stuttgart-ipr-assignments/hft-asgmt-html.svg?branch=master)](https://travis-ci.org/hft-stuttgart-ipr-assignments/hft-asgmt-html)
+
+# hft-asgmt-html
+
+## TODO
+- index.html:
+  - [ ] Add style.css
+  - [ ] Reference `style.css` in html file
+  - [ ] Use `display: float;` on body
+  - [ ] Style the table somehow
+  - [ ] Use a background image somewhere that does not repeat
+  - [ ] Use a hover effekt on the link
+  - [ ] Use another font
+
---
+++ test/index.spec.js
@@ -0,0 +1,65 @@
+import puppeteer from 'puppeteer'
+import path from 'path'
+import fs from 'fs'
+
+let page;
+let browser;
+const styles = `file://${path.resolve('style.css')}`
+const indexPage = `file://${path.resolve('index.html')}`
+const width = 1440;
+const height = 900;
+
+describe('index HTML', () => {
+  beforeAll(async () => {
+    browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    page = await browser.newPage();
+    await page.setViewport({ width, height });
+  });
+  afterAll(async () => {
+    await page.screenshot({path: 'index.png'});
+    browser.close();
+  });
+
+  it('style.css should exist', () => {
+    expect(styles).not.toBeNull();
+  });
+
+  it('Should use \'display: float\' on body ', (done) => {
+    fs.readFile(path.resolve('style.css'), 'utf8', function (err,data) {
+      expect(data.indexOf('display') > -1).toBeTruthy()
+      expect(data.indexOf('float') > -1).toBeTruthy()
+      done()
+    });
+  });
+
+  it('Should style the table', (done) => {
+    fs.readFile(path.resolve('style.css'), 'utf8', function (err,data) {
+      expect(data.indexOf('table') > -1).toBeTruthy()
+      done()
+    });
+  });
+
+  it('Should have a background-image with no-repeat', (done) => {
+    fs.readFile(path.resolve('style.css'), 'utf8', function (err,data) {
+      expect(data.indexOf('no-repeat') > -1).toBeTruthy()
+      done()
+    });
+  });
+
+  it('Should use hover effect on a link', (done) => {
+    fs.readFile(path.resolve('style.css'), 'utf8', function (err,data) {
+      expect(data.indexOf('a:hover') > -1).toBeTruthy()
+      done()
+    });
+  });
+
+  it('Should use another font', (done) => {
+    fs.readFile(path.resolve('style.css'), 'utf8', function (err,data) {
+      expect(data.indexOf('font-family') > -1).toBeTruthy()
+      done()
+    });
+  });
+});
